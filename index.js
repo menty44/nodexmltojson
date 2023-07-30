55c9bb866da6d7ffa12cfb70e87f8d3301b5aa78
Fredrick Oluoch <menty44@gmail.com>
2023-07-30 12:59:22 +0300

    parsed xml to json

--- index.js
+++ index.js
@@ -1,6 +1,45 @@
 var express = require('express');
 var app = express();
 
+// import File System Module
+var fs = require("fs");
+
+// import xml2js Module
+var { parseString }  = require("xml2js");
+
+//xml data
+var xmldata = '<?xml version=”1.0" encoding=”UTF-8"?>' +
+    '<Student>' +
+    '<PersonalInformation>' +
+    '<FirstName>Sravan</FirstName>' +
+    '<LastName>Kumar</LastName>' +
+    '<Gender>Male</Gender>' +
+    '</PersonalInformation>' +
+    '<PersonalInformation>' +
+    '<FirstName>Sudheer</FirstName>' +
+    '<LastName>Bandlamudi</LastName>' +
+    '<Gender>Male</Gender>' +
+    '</PersonalInformation>' +
+    '</Student>';
+
+// parsing xml data
+
+fs.readFile( '/Users/mac/Documents/testEmtechBatch-ptstmts.xml', function (err, data) {
+   if (err) {
+      throw err;
+   }
+   // console.log(data.toString());
+   parseString(data, function (err, results) {
+
+// parsing to json
+      let data = JSON.stringify(results)
+
+// display the json data
+      console.log("results",data);
+   });
+});
+
+
 app.get('/', function(req, res){
    res.send("Hello world!");
 });
