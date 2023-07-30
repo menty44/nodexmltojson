86a8a2aa2ec6c1a971b793f6b810eedec7a32d1f
Fredrick Oluoch <menty44@gmail.com>
2023-07-30 13:35:08 +0300

    Formatted code

--- index.js
+++ index.js
@@ -6,43 +6,27 @@ var fs = require("fs");
 require('dotenv').config()
 
 // import xml2js Module
-var { parseString }  = require("xml2js");
-
-//xml data
-var xmldata = '<?xml version=”1.0" encoding=”UTF-8"?>' +
-    '<Student>' +
-    '<PersonalInformation>' +
-    '<FirstName>Sravan</FirstName>' +
-    '<LastName>Kumar</LastName>' +
-    '<Gender>Male</Gender>' +
-    '</PersonalInformation>' +
-    '<PersonalInformation>' +
-    '<FirstName>Sudheer</FirstName>' +
-    '<LastName>Bandlamudi</LastName>' +
-    '<Gender>Male</Gender>' +
-    '</PersonalInformation>' +
-    '</Student>';
+var {parseString} = require("xml2js");
 
 // parsing xml data
-
-fs.readFile( process.env.FILE_PATH, function (err, data) {
-   if (err) {
-      throw err;
-   }
-   // console.log(data.toString());
-   parseString(data, function (err, results) {
+fs.readFile(process.env.FILE_PATH, function (err, data) {
+    if (err) {
+        throw err;
+    }
+    // console.log(data.toString());
+    parseString(data, function (err, results) {
 
 // parsing to json
-      let data = JSON.stringify(results)
+        let data = JSON.stringify(results)
 
 // display the json data
-      console.log("results",data);
-   });
+        console.log("results", data);
+    });
 });
 
 
-app.get('/', function(req, res){
-   res.send("Hello world!");
+app.get('/', function (req, res) {
+    res.send("Hello world!");
 });
 
 app.listen(3000);
